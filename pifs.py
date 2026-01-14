ea21d9f8dc7d766cf406242e251149951701248b
pepperosavr <kastefanova.work@gmail.com>
2026-01-14 11:13:56 +0300

    Update pifs.py

--- pifs.py
+++ pifs.py
@@ -248,6 +248,7 @@ if mode == "Режим истории":
 
 # Показываем изменение цены как процент в hover
     fig_close_pct.update_yaxes(hoverformat=".2f")
+    fig_close_pct.update_layout(separators=". ")
     
     fig_close_pct.update_traces(
         hovertemplate=(
